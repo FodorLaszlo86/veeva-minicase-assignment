1f2cb3d20924e83c8fa7e83c1606c04d2f4cc904
Fodor Laszlo <fodor.laszlo86@gmail.com>
2019-03-18 21:31:00 +0100

    Updates

---
+++ precache-manifest.3119b1f1cd7e050bf3a1ae0c471e3f58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f92922dd1edce7977e3",
+    "url": "/veeva-minicase-assignment/static/js/runtime~main.d7129a3d.js"
+  },
+  {
+    "revision": "0b7cabef06266d091a1f",
+    "url": "/veeva-minicase-assignment/static/js/main.b5377e6d.chunk.js"
+  },
+  {
+    "revision": "dec1e280990a6aa698a2",
+    "url": "/veeva-minicase-assignment/static/js/2.faccd353.chunk.js"
+  },
+  {
+    "revision": "0b7cabef06266d091a1f",
+    "url": "/veeva-minicase-assignment/static/css/main.31aa633c.chunk.css"
+  },
+  {
+    "revision": "8d36b311f48c0f62df6d856f020db783",
+    "url": "/veeva-minicase-assignment/index.html"
+  }
+];
